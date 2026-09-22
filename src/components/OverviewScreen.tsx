d1354b3164d85e15ecf0867404846e9189256b56
Svetlana Andreeva <AnbreevaSveta@yandex.ru>
2026-09-22 12:05:59 +0500

    Connect finance contexts to overview pages

--- src/components/OverviewScreen.tsx
+++ src/components/OverviewScreen.tsx
@@ -1,16 +1,18 @@
 import { CashflowChart } from './CashflowChart'
+import { ContextSummary } from './ContextSummary'
 import { ForecastCard } from './ForecastCard'
 import { HeroBalance } from './HeroBalance'
 import { MonthlyMetrics } from './MonthlyMetrics'
 import { QuickActions } from './QuickActions'
 import { TransactionsList } from './TransactionsList'
 
-export function OverviewScreen({ show, onToggleShow }: { show: boolean; onToggleShow: () => void }) {
+export function OverviewScreen({ show, onToggleShow, ctx }: { show: boolean; onToggleShow: () => void; ctx: string }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
-      <HeroBalance show={show} onToggleShow={onToggleShow} />
+      <ContextSummary ctx={ctx} />
+      <HeroBalance show={show} onToggleShow={onToggleShow} ctx={ctx} />
       <QuickActions />
-      <MonthlyMetrics show={show} />
+      <MonthlyMetrics show={show} ctx={ctx} />
       <CashflowChart />
       <ForecastCard show={show} />
       <TransactionsList show={show} />
