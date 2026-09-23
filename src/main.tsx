0be17f68a7237a3c34a8340e361288b3cc00000d
Svetlana Andreeva <AnbreevaSveta@yandex.ru>
2026-09-23 09:57:20 +0500

    Wire one-click Ozon sync into MoneyCRM

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthGate } from './components/AuthGate'
+import { OzonQuickSync } from './components/OzonQuickSync'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthGate>
       <App />
+      <OzonQuickSync />
     </AuthGate>
   </React.StrictMode>,
 )
